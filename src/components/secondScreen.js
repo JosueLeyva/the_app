3895a031804aa75f7860354f83d4d5fd9cffca9c
Josue Leyva <Josue@college10-100-64-59-52.resnet.ucsc.edu>
2019-05-24 09:46:39 -0700

    added Home + Second screen

---
+++ src/components/secondScreen.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import {
+  View,
+  Text,
+} from 'react-native';
+
+class secondScreen extends React.Component {
+  render(){
+    return(
+      <View>
+        <Text>
+          Hello secondScreen
+        </Text>
+      </View>
+    );
+  }
+}
+
+export default secondScreen;
