95a5581e61f8221d38102a3ebd693f4416ab13e7
Josue Leyva <Josue@college10-100-64-59-52.resnet.ucsc.edu>
2019-05-24 00:25:12 -0700

    add name

--- App.js
+++ App.js
@@ -4,6 +4,11 @@ import { StyleSheet, Text, View } from 'react-native';
 export default class App extends React.Component {
   render() {
     return (
+      <View style={{flex:1, justifyContent:"center", alignItems:"center"}}>
+      <Text>Hello, Josue!</Text>
+      </View>
+
+
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
       </View>
