592e68ad4303ee9bf0e18f489c41f03e7181d4fd
Josue Leyva <Josue@eduroam-169-233-160-227.ucsc.edu>
2019-05-23 21:12:31 -0700

    First Commit

---
+++ main1.java
@@ -0,0 +1,17 @@
+public class main1{
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+    //comment
+    //added
+    String hell = "Hell";
+    int x = 70;
+    double twenty = 20;
+    //this is it
+    //another one
+    int three = 3 ;
+   }
+   public static void name1(){
+        int mult = 5*2; //10
+        double mult;
+   }
+}
