4a53d923c44fbfbb784d49472b1a877f73301c13
Josue Leyva <Josue@college10-100-64-59-52.resnet.ucsc.edu>
2019-05-25 23:54:55 -0700

    App class

--- App.js
+++ App.js
@@ -1,8 +1,26 @@
 import React from 'react';
-import Home from '.Component/Home'
+import Home from '.Component/Home';
 import secondScreen from '.Component/secondScreen';
 
+import{
+  Router,
+  Scene,
+} from 'react-native-router-flux';
+
 import { StyleSheet, Text, View } from 'react-native';
+
+class App extends React.Component{
+  render(){
+    return{
+      <Router>
+        <Scene key='root'>
+          <Scene key='home' component={Home} title='Home'/>
+          <Scene key='secondScreen' component={secondScreen} title='secondScreen'/>
+        </Scene>
+      </Router>
+    };
+  }
+}
 
 export default class App extends React.Component {
   render() {
@@ -29,3 +47,6 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+
+export default App;
